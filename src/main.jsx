15ad6a332e82d80128f14c66f6cbd99c30239329
Joao Vitor <133013977+VitorAraujo98@users.noreply.github.com>
2023-06-13 20:30:05 -0300

    rotas commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'  // Rotas //
+
+import App from './App.jsx'
+
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <BrowserRouter>
+   <Routes>
+    <Route element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<Movie />} />    // Rotas do projeto //
+      <Route path="search" element={<Search />} />
+    </Route>
+   </Routes>
+   </BrowserRouter>
+  </React.StrictMode>
+)
